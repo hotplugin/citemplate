fa9c9eaf345bbdad49bf574e486275dd3fdca8bf
faisal adil <hotpluginenator@gmail.com>
2013-07-10 12:08:48 +0545

    login page

--- application/modules/login/views/login.php
+++ application/modules/login/views/login.php
@@ -3,5 +3,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'loginview';
+
+$this->load->helper('form');
+echo validation_errors();
+echo form_open('login');
+echo form_input('username');
+echo form_password('password');
+echo form_submit('submit', 'Login');
+echo form_close();
 ?>
