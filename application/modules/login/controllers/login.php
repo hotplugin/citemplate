98bc1242ff22d69baff635fd21bf817b67d7b8f5
faisal adil <hotpluginenator@gmail.com>
2013-07-10 12:07:39 +0545

    login controller

--- application/modules/login/controllers/login.php
+++ application/modules/login/controllers/login.php
@@ -12,9 +12,44 @@ use login\models\User;
 class Login extends MX_Controller {
 
     public function index() {
-         $this->load->helper('url');
-          $data['view_page'] ='login';
-           $this->load->view('template/master', $data);
+//         $this->load->helper('url');
+//         $data['title'] = 'login';
+//          $data['view_page'] ='login';
+//           $this->load->view('template/master', $data);
+        $this->load->helper('url');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        if ($this->form_validation->run() === FALSE) {
+            $data['title'] = 'login';
+            $data['view_page'] = 'login';
+            $this->load->view('template/master', $data);
+        } else {
+            $this->load->model('login/login_model', 'user');
+            //check username taken n Password
+            $data['userrow'] = $this->user->checkUsernameNPassword($this->input->post('username'),$this->input->post('password'));
+            if (!empty($data['userrow'])) {
+                echo 'validated';
+                return;
+            }else{
+                echo 'invalid';
+            }
+//            $this->load->library('doctrine');
+//            $usernew = new register\models\User;
+//            $usernew->setName($this->input->post('name'));
+//            $usernew->setAddress($this->input->post('address'));
+//            $usernew->setUsername($this->input->post('username'));
+//            $usernew->setPassword($this->input->post('password'));
+//            $this->doctrine->em->persist($usernew);
+//            $this->doctrine->em->flush();
+//            if ($usernew->id()) {
+//                $data['view_page'] = 'formsuccess';
+//                $data['title'] = 'Registerd Success!';
+//                $this->load->view('template/master', $data);
+//            } else {
+//                echo "error in data entry";
+//            }
+        }
     }
 
 }
